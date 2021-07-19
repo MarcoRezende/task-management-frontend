75cc2f88aa8fec119bee34c2a47a17b241a4d2ff
MarcoRezende <MarcoRezende@users.noreply.github.com>
2021-07-19 16:04:59 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.89008af61dbb157380f60ee0f0ab80ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a378066ffedbc0a1c1e601017835d455",
+    "url": "https://marcorezende.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "787a46756e8e3a5563e6",
+    "url": "https://marcorezende.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "c0cc16839966dc6a1570",
+    "url": "https://marcorezende.github.io/task-management-frontend/static/js/2.e94175e8.chunk.js"
+  },
+  {
+    "revision": "787a46756e8e3a5563e6",
+    "url": "https://marcorezende.github.io/task-management-frontend/static/js/main.fe390c63.chunk.js"
+  },
+  {
+    "revision": "05361bb4a0e31a3c37f2",
+    "url": "https://marcorezende.github.io/task-management-frontend/static/js/runtime~main.e8395ad8.js"
+  }
+]);
